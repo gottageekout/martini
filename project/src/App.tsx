aeb58e75786289da2d62f9531963f6afca9dcf93
Sivakumar <sivakumar@example.com>
2026-05-13 13:17:08 +0530

    feat: add lenis smooth scroll effect

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import Lenis from 'lenis'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import PulseBanner from './components/PulseBanner/PulseBanner'
@@ -7,6 +9,30 @@ import Brands from './components/Brands/Brands'
 import Footer from './components/Footer/Footer'
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      orientation: 'vertical',
+      gestureOrientation: 'vertical',
+      smoothWheel: true,
+      wheelMultiplier: 1,
+      touchMultiplier: 2,
+      infinite: false,
+    })
+
+    function raf(time: number) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+
+    requestAnimationFrame(raf)
+
+    return () => {
+      lenis.destroy()
+    }
+  }, [])
+
   return (
     <div className="app-container">
       <Header />
